c563b789075b6f42db87a11fe05de116fa3bd19e
sotatek-giangdo <giang.do@sotatek.com>
2024-06-09 16:34:25 +0700

    page 1

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+function loadContent() {
+    var header = 'header';
+    $(".header").load('file:///E:/xampp/htdocs/email-mkt/template/' + header + '.html');
+    console.log('file:///E:/xampp/htdocs/email-mkt/template/header.html');
+}
