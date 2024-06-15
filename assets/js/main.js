cc2728f71839f9134474a5d6b68cd7d899a73e96
sotatek-giangdo <giang.do@sotatek.com>
2024-06-15 21:52:47 +0700

    file 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,12 @@
 function loadContent() {
-    var header = 'header';
-    $(".header").load('file:///E:/xampp/htdocs/email-mkt/template/' + header + '.html');
-    console.log('file:///E:/xampp/htdocs/email-mkt/template/header.html');
+    jQuery(document).ready(function ($) {
+        $('.table-data').each(function () {
+            var tr = $(this).find('tr:last-child');
+            var tdCount = tr.find('td').length;
+            var tdWidth = 100 / (tdCount + 1);
+            $(this).find('tr td:not(td:first-of-type)').css('width', tdWidth + '%');
+        });
+    });
 }
+
+loadContent();
